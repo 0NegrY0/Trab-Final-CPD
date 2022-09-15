f419cd8947ed7fffd585715a0b6d9faa60801e5a
Leokleffa <102036917+Leokleffa@users.noreply.github.com>
2022-09-15 20:11:09 -0300

    Add files via upload

--- Final-CPD/Include/trie.h
+++ Final-CPD/Include/trie.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-#include <bits/types/FILE.h>  //TALVEZ MUDAR DPS
+//#include <bits/types/FILE.h>  //TALVEZ MUDAR DPS
 
 #define BUFFER_MAX 400
 #define NAME_MAX 200
@@ -16,35 +16,18 @@ using namespace std;
 class Trie
 {
 	public:
-		
-		void saveTrie(char name[NAME_MAX], long position, FILE *trie_tree);
-		void searchByName(char nomeProcurado[NAME_MAX]);
-	
-	
-
+	    void iniciaTrie();
+		void saveTrie(char name[NAME_MAX], long position, fstream trieBin);
+		void searchByName(char nomeProcurado[NAME_MAX], fstream trieBin);
 	private:
 		typedef struct{
-		
 			long file_position;
 			char letter;
 
 			long left_pos;
 			long son_pos;
 			long right_pos;
-		} Trie_node
-
-
-		// Constructor
-		/*Trie(){
-			index = 0;
-			this->isLeaf = false;
-			for (int i = 0; i < CHAR_SIZE; i++)
-				this->character[i] = nullptr;
-		}
-		void insert(string);
-		bool deletion(Trie*&, string);
-		bool search(string);
-		bool haveChildren(Trie const*);*/
+		} Trie_node;
 };
 
 #endif // TRIE_H
