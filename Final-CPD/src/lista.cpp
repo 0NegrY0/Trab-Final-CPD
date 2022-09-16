1541e57fc94792b66163f22a5aeaabed2e50d8bc
NegrY <48329860+0NegrY0@users.noreply.github.com>
2022-09-16 18:03:27 -0300

    Update lista.cpp

--- Final-CPD/src/lista.cpp
+++ Final-CPD/src/lista.cpp
@@ -24,6 +24,34 @@ long saveList(Registro data, fstream listaBin){
     return filePosition;
 }
 
+Registro leLista(long filePosition, fstream listaBin){
+    bool found = false;
+    Registro procurado;
+
+    while( !found )
+    {
+        listaBin.read((char *)&procurado, sizeof procurado);
+        if( listaBin.eof() )  // eof bit gets set only after EOF is read from file
+        break;
+
+        if(procurado.id = file)     //MUDAR
+        {
+        // handle found record
+        found = true;
+        }
+    }
+    if( !found )
+    {
+        cerr << "Could not find the id - " << filePosition << "on the list" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else{
+        std::cout << procurado << endl;
+    }
+
+}
+
+
 void closeLista(fstream listaBin){
     listaBin.close();
 }
